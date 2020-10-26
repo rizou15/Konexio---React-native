00206ed1b8b09888ba89748d39927fb589de15ba
rizou15 <rizou1504@gmail.com>
2020-10-26 16:59:02 +0100

    exercice_1 button item

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native';
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View>
+        <Image source={require('./assets/konexio-logo_1.png')}
+               style={{ width: 150, height: 50 }} />
+        <Image source={{ uri:'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png' }}
+               style={{ width: 150, height: 50 }} />
+        <Button class="alert alert-primary" role="alert"></Button>     
+        <Text style={{ fontSize: 30 }}>The original</Text>
+        <Text style={{ textAlign: 'center' }}>Worldwide</Text>
+        <Text><b>My first React native app !</b></Text>
+      </View>
+    </ScrollView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'violet',
+    padding: 80,
+    margin: 10
   },
 });
