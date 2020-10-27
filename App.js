0f82ea011b0e6c183a34c061f96a2d18b085b3b5
rizou15 <rizou1504@gmail.com>
2020-10-27 16:54:23 +0100

    fin exrecice_1

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native';
+import Paragraphe from './src/component/Paragraphe';
+import { StyleSheet, Text, View, ScrollView, Image, Button, ActivityIndicator, } from 'react-native';
+
 
 
 
@@ -9,14 +11,21 @@ export default function App() {
   return (
     <ScrollView style={styles.container}>
       <View>
-        <Image source={require('./assets/konexio-logo_1.png')}
-               style={{ width: 150, height: 50 }} />
-        <Image source={{ uri:'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png' }}
-               style={{ width: 150, height: 50 }} />
-        <Button class="alert alert-primary" role="alert"></Button>     
-        <Text style={{ fontSize: 30 }}>The original</Text>
-        <Text style={{ textAlign: 'center' }}>Worldwide</Text>
-        <Text><b>My first React native app !</b></Text>
+        <View style={styles.container2}>
+          <Image source={require('./assets/konexio-logo_1.png')}
+                 style={{ width: 150, height: 50 }} />
+          <Image source={{ uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png' }}
+                 style={{ width: 150, height: 50 }} />
+        </View>
+        <Button title="Enter"
+                onPress={() => alert('Button color pressed')} />
+        <Text style={styles.text1}>My first React native app !</Text>
+        <Text style={styles.text2}>Original</Text>
+        <Text style={styles.text3}>Made by myself</Text>
+        <ActivityIndicator size="large" color="#00ff00" />
+        <View style={styles.container3}>
+          <Paragraphe />
+        </View>
       </View>
     </ScrollView>
 
@@ -26,8 +35,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'violet',
-    padding: 80,
-    margin: 10
+    backgroundColor: 'darkturquoise',
   },
+  text1: {
+    flex: 1,
+    fontSize: 30,
+    textAlign: 'center',
+    marginVertical: 35
+  },
+  text2: {
+    flex: 1,
+    textAlign: 'center',
+    marginVertical: 35
+  },
+  text3: {
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginVertical: 35
+  },
+  button: {
+    color: '#f194ff'
+  },
+  container2: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  container3: {
+    justifyContent: 'justify'
+  }
+
 });
+
+
+
+
